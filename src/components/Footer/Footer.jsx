1390f8d03a095106bb200bcfb8f7a499cc59f71d
kmositua <kmositua@gmail.com>
2024-08-31 12:48:52 +0100

    create components in separate modules

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <footer>
+      <span>&copy; All rights reserved! 2024</span>
+    </footer>
+  );
+};
+export default Footer;
---
+++ src/components/WelcomeSection/WelcomeSection.jsx
@@ -0,0 +1,37 @@
+const WelcomeSection = () => {
+  return (
+    <div>
+      <section>
+        <h2>Welcome to React</h2>
+        <ul>
+          <li>Hello</li>
+        </ul>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </section>
+      <section>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
+          reiciendis harum id amet? Maiores facere, voluptatem, eveniet
+          distinctio aliquam porro repudiandae tempora pariatur magni, omnis
+          deleniti nisi praesentium consectetur quae.
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet,
+          mollitia maiores? Doloribus odio natus pariatur quidem veritatis earum
+          dolorem temporibus. Earum quisquam dignissimos voluptates molestias
+          voluptatibus quia, est veritatis id. Quae, numquam, consequatur iure
+          libero molestias non esse, quis dolorum hic neque aliquid quisquam!
+          Officiis exercitationem provident odit reiciendis ad temporibus
+          praesentium nisi mollitia? Quidem eum totam voluptate neque
+          distinctio! Laborum ex facere cupiditate? Unde illo recusandae vitae
+          excepturi quaerat et, totam quam maxime. Minus sit officiis adipisci
+          quasi nobis! Fugiat magnam autem voluptatem eum, impedit nostrum ex
+          laudantium labore?
+        </p>
+      </section>
+    </div>
+  );
+};
+export default WelcomeSection;
